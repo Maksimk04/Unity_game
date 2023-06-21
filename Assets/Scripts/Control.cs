219efd36abaeaaf83d5c44227e8c45c00fde42e2
Hiricus <hexaeder@yandex.ru>
2023-06-21 03:48:05 +0300

    Added player turns

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
+        {
             MovePlayer(Vector3.up);
-
+        }
         if (Input.GetKeyDown(KeyCode.S))
+        {
             MovePlayer(Vector3.down);
-
+        }
         if (Input.GetKeyDown(KeyCode.A))
+        {
             MovePlayer(Vector3.left);
-
+            sprite.flipX = false;
+        }
         if (Input.GetKeyDown(KeyCode.D))
+        {
             MovePlayer(Vector3.right);
+            sprite.flipX = true;
+        }
     }
 
     private void MovePlayer(Vector3 direction)
