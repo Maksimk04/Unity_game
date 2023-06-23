fd5d1358a7a7ad4d2e84fe715eb423dc0c249bb8
Maksimk04 <yarovoy.mr@yandex.ru>
2023-06-23 18:15:54 +0300

    Changed movement

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
     SpriteRenderer sprite;
+    private bool Moving = false;
 
     void Start()
     {
@@ -13,31 +14,51 @@ public class Control : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && !Moving)
         {
-            MovePlayer(Vector3.up);
+            StartCoroutine(MovePlayer(Vector3.up));
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) && !Moving)
         {
-            MovePlayer(Vector3.down);
+            StartCoroutine(MovePlayer(Vector3.down));
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) && !Moving)
         {
-            MovePlayer(Vector3.left);
+            StartCoroutine(MovePlayer(Vector3.left));
             sprite.flipX = false;
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) && !Moving)
         {
-            MovePlayer(Vector3.right);
+            StartCoroutine(MovePlayer(Vector3.right));
             sprite.flipX = true;
         }
     }
 
-    private void MovePlayer(Vector3 direction)
+    private IEnumerator MovePlayer(Vector3 direction)
     {
+        Moving = true;
+        float time = 0;
+        Vector3 PosStart = transform.position;
+        Vector3 Pos = PosStart + direction;
+
         if (Movement.Move(direction))
         {
-            transform.position = transform.position + direction;
+            //transform.position = transform.position + direction;
+            while (time  < 0.15f)
+            {
+                transform.position = Vector3.Lerp(PosStart, Pos, (time / 0.15f));
+                time+= Time.deltaTime;
+                yield return null;
+            }
+            transform.position = Pos;
+            time = 0;
+            while (time < 0.01f)
+            {
+                //transform.position = Vector3.Lerp(BoxPos, Pos, (time / 0.15f));
+                time += Time.deltaTime;
+                yield return null;
+            }
         }
+        Moving = false;
     }
 }
